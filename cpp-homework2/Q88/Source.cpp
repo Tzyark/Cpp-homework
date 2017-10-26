e2b8b468c528b01f2627e4a13034bbf71f3375c6
jstztzy <jstztzy@gmail.com>
2017-10-26 10:56:11 +0800

    fix Q88

--- cpp-homework2/Q88/Source.cpp
+++ cpp-homework2/Q88/Source.cpp
@@ -1,46 +1,33 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <time.h>
 using namespace std;
 
 const int N = 10009;
 
 int n;
-string str[N];
+string s[N];
 
-inline int hashing(const char &c) {
-	return 2 * (tolower(c) - 'a') + isupper(c);
+int hashing(const char &c) {
+	return 2 * (toupper(c) - 'A') + isupper(c);
+}
+
+bool smaller(const string &s1, const string &s2) {
+	for (int i = 0; i < s1.size(); i++) {
+		if (hashing(s1[i]) < hashing(s2[i])) return true;
+		if (hashing(s1[i]) > hashing(s2[i])) return false;
+	}
+	return true;
 }
 
 bool cmp(const string &s1, const string &s2) {
-	if (s1.size() < s2.size())
-		return true;
-	else 
-		if (s1.size() == s2.size()) {
-			for (int i = 0; i < s1.size(); i++) {
-				if (hashing(s1[i]) < hashing(s2[i]))
-					return true;
-				else
-					return false;
-			}
-		}
-		else
-			return false;
+	return s1.size() < s2.size() || s1.size() == s2.size() && smaller(s1, s2);
 }
 
 int main() {
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> str[i];
-	/*
-	srand((unsigned) time(0));
-	ofstream fout;
-	string filename = "e:\\data"; filename += rand() % 10 + 48; filename += ".txt";
-	fout.open(filename);
-	//for (int i = 0; i < n; i++) fout << str[i] << endl;
-	fout.close();
-	*/
-	sort(str, str + n, cmp);
-	for (int i = 0; i < n; i++) cout << str[i] << endl;
+	for (int i = 0; i < n; i++) cin >> s[i];
+	sort(s, s + n, cmp);
+	for (int i = 0; i < n - 1; i++) cout << s[i] << endl;
+	cout << s[n - 1];
 }
